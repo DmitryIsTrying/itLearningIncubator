2c35d5cfc7674b763f6cd6041c7cf1d5492b189c
Dmitry <d2013.dima@mail.ru>
2024-07-10 20:47:50 +0500

    Изучил массивы, изменение отдельных элементов массива

--- js-tusk1.js
+++ js-tusk1.js
@@ -5,3 +5,8 @@ character = 'World';
 // Создание массива, вывод первого элемента массива в консоль
 let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
+// Изменение последнего элемента массива с помощью length
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
