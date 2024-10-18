3891407d5331e84d995ff518db1ed3c78c131545
Dmitry <d2013.dima@mail.ru>
2024-10-18 18:25:53 +0500

    2 kata's

---
+++ codewars/HumanReadable.js
@@ -0,0 +1,41 @@
+function formatDuration(seconds) {
+  if (!seconds) return 'now'
+  let years = 0
+  let days = 0
+  let hours = 0
+  let minute = 0
+  let sec = 0
+
+  years = Math.floor(seconds / 31536000)
+
+  const DHMS = seconds - years * 31536000
+
+  days = Math.floor(DHMS / 86400)
+  const HMS = DHMS - days * 86400
+  hours = Math.floor(HMS / 3600)
+  const MS = HMS - hours * 3600
+  minute = Math.floor(MS / 60)
+  sec = MS - minute * 60
+  return readData(years, days, hours, minute, sec)
+}
+
+function readData(...times) {
+  const arrayData = ['year', 'day', 'hour', 'minute', 'second']
+  const newSortedData = []
+
+  times = times.filter((num, i) => {
+    if (num > 0) {
+      newSortedData.push(arrayData[i])
+      return true
+    }
+  })
+
+  let resultStr = ''
+  for (let i = 0; i < times.length; i++) {
+    resultStr += times[i] + ' ' + newSortedData[i] + (times[i] > 1 ? 's' : '')
+    resultStr += i + 2 === times.length ? ' and ' : i + 1 === times.length ? '' : ', '
+  }
+  return resultStr
+}
+
+console.log(formatDuration(132030240))
--- codewars/snailSort.js
+++ codewars/snailSort.js
@@ -1,39 +1,75 @@
-snail = function (array) {
-  const result = []
-  for (let i = 0; i < array.length / 2; i++) {
-    // x direction
-    result.push(array[i].slice(i === 0 ? i : i - 1, array[i].length - i))
-    // y direction
-    let j = array.length - 1 - i
-    let reverseArr = []
-    while (j > i) {
-      reverseArr.push(array[j][array.length - 1 - i])
+// snail = function (array) {
+//   const result = []
+//   for (let i = 0; i < array.length / 2; i++) {
+//     // x direction
+//     result.push(array[i].slice(i === 0 ? i : i - 1, array[i].length - i))
+//     // y direction
+//     let j = array.length - 1 - i
+//     let reverseArr = []
+//     while (j > i) {
+//       reverseArr.push(array[j][array.length - 1 - i])
 
-      j--
+//       j--
+//     }
+//     result.push(reverseArr.reverse())
+//     // x reverse direction
+//     result.push(array[array.length - 1 - i].slice(i, array[i].length - i - 1).reverse())
+//     // y reverse direction
+//     let m = array.length - 2 - i
+//     let arrY = []
+//     while (m > i + 1) {
+//       arrY.push(array[m][i])
+//       m--
+//     }
+//     result.push(arrY)
+//   }
+
+//   return result.flat()
+// }
+
+// console.log(
+//   snail([
+//     [1, 2, 3, 4, 5, 6],
+//     [20, 21, 22, 23, 24, 7],
+//     [19, 32, 33, 34, 25, 8],
+//     [18, 31, 36, 35, 26, 9],
+//     [17, 30, 29, 28, 27, 10],
+//     [16, 15, 14, 13, 12, 11],
+//   ])
+// )
+
+function snail(array) {
+  let result = []
+
+  while (array.length) {
+    // Берем и удаляем первый ряд
+    result = result.concat(array.shift())
+
+    // Берем последний элемент каждого из оставшихся рядов
+    for (let i = 0; i < array.length; i++) {
+      result.push(array[i].pop())
     }
-    result.push(reverseArr.reverse())
-    // x reverse direction
-    result.push(array[array.length - 1 - i].slice(i, array[i].length - i - 1).reverse())
-    // y reverse direction
-    let m = array.length - 2 - i
-    let arrY = []
-    while (m > i + 1) {
-      arrY.push(array[m][i])
-      m--
+
+    // Берем и удаляем последний ряд в обратном порядке
+    if (array.length) {
+      result = result.concat(array.pop().reverse())
     }
-    result.push(arrY)
+
+    // Берем первый элемент каждого оставшегося ряда в обратном порядке
+    for (let i = array.length - 1; i >= 0; i--) {
+      result.push(array[i].shift())
+    }
   }
 
-  return result.flat()
+  return result
 }
 
-console.log(
-  snail([
-    [1, 2, 3, 4, 5, 6],
-    [20, 21, 22, 23, 24, 7],
-    [19, 32, 33, 34, 25, 8],
-    [18, 31, 36, 35, 26, 9],
-    [17, 30, 29, 28, 27, 10],
-    [16, 15, 14, 13, 12, 11],
-  ])
-)
+// Пример использования:
+const array1 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]
+
+console.log(snail(array1)) // [1, 2, 3, 6, 9, 8, 7, 4, 5]
+console.log(array1)
