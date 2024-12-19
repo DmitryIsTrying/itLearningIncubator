3127149fa284965f33fed6e19ea427bf26ab6f05
Dmitry <d2013.dima@mail.ru>
2024-12-20 01:58:51 +0500

    codewars

---
+++ codewars/alternate.js
@@ -0,0 +1,13 @@
+function isAlt(word) {
+  const arr = ['a', 'e', 'i', 'o', 'u']
+  for (let i = 0; i < word.length - 1; i++) {
+    if (!arr.includes(word[i]) && !arr.includes(word[i + 1])) {
+      return false
+    }
+    if (arr.includes(word[i]) && arr.includes(word[i + 1])) {
+      return false
+    }
+  }
+  return true
+}
+console.log(isAlt('ceebi'))
