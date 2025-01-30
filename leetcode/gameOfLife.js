1ec596c853b11dd610e7997c46587240e98f8186
Dmitry <d2013.dima@mail.ru>
2025-01-30 22:36:31 +0500

    leetcode

--- leetcode/gameOfLife.js
+++ leetcode/gameOfLife.js
@@ -1,7 +1,36 @@
 var gameOfLife = function (board) {
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index]
+  const newState = []
+  for (let i = 0; i < board.length; i++) {
+    let row = []
+    for (let j = 0; j < board[0].length; j++) {
+      row.push(nextCellState(board, { i, j }))
+    }
+    newState.push(row)
   }
+  board.length = 0
+  board.push(...newState)
+}
+
+function nextCellState(board, { i, j }) {
+  const roommates = []
+
+  roommates.push(board[i - 1]?.[j - 1] || 0)
+  roommates.push(board[i - 1]?.[j + 1] || 0)
+  roommates.push(board[i - 1]?.[j] || 0)
+  roommates.push(board[i]?.[j - 1] || 0)
+  roommates.push(board[i]?.[j + 1] || 0)
+  roommates.push(board[i + 1]?.[j - 1] || 0)
+  roommates.push(board[i + 1]?.[j + 1] || 0)
+  roommates.push(board[i + 1]?.[j] || 0)
+
+  const allive = roommates.filter((num) => num === 1)
+  if (allive.length < 2 || allive.length > 3) {
+    return 0
+  }
+
+  if (allive.length === 3) return 1
+
+  return board[i][j]
 }
 const board = [
   [0, 1, 0],
@@ -9,4 +38,5 @@ const board = [
   [1, 1, 1],
   [0, 0, 0],
 ]
-console.log(gameOfLife(board))
+gameOfLife(board)
+console.log(board)
