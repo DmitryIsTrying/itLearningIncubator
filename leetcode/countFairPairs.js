9d7ef1b6dca330cbeab6dbfdbbc36f29812a3a8d
Dmitry <d2013.dima@mail.ru>
2025-04-19 21:13:26 +0500

    leetcode

---
+++ leetcode/countFairPairs.js
@@ -0,0 +1,23 @@
+var countFairPairs = function (nums, lower, upper) {
+  nums.sort((a, b) => a - b)
+
+  const countPairsWithSumLessThan = (target) => {
+    let left = 0,
+      right = nums.length - 1
+    let count = 0
+
+    while (left < right) {
+      const sum = nums[left] + nums[right]
+      if (sum < target) {
+        count += right - left
+        left++
+      } else {
+        right--
+      }
+    }
+
+    return count
+  }
+
+  return countPairsWithSumLessThan(upper + 1) - countPairsWithSumLessThan(lower)
+}
